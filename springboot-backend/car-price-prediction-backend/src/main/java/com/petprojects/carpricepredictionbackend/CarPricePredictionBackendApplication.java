9733273c28710d088d747e603d6509559abc6c58
TySamTakoy <brat.sumkina@gmail.com>
2026-03-26 01:57:47 +0200

    init: project start

---
+++ springboot-backend/car-price-prediction-backend/src/main/java/com/petprojects/carpricepredictionbackend/CarPricePredictionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.petprojects.carpricepredictionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPricePredictionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarPricePredictionBackendApplication.class, args);
+    }
+
+}
---
+++ springboot-backend/car-price-prediction-backend/src/test/java/com/petprojects/carpricepredictionbackend/CarPricePredictionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petprojects.carpricepredictionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPricePredictionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
