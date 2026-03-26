26235510cc03b0eb6c689c70186dd998ee1d07e1
TySamTakoy <brat.sumkina@gmail.com>
2026-03-26 16:08:03 +0200

    init: add mappings

---
+++ python-services/ml-service/ml-initial-price-prediction/mappings.py
@@ -0,0 +1,36 @@
+ENGINE_TYPE_MAP = {
+    "PETROL": "бензин",
+    "DIESEL": "дизель",
+    "HYBRID": "гибрид",
+    "ELECTRIC": "электро",
+}
+
+TRANSMISSION_MAP = {
+    "MANUAL": "механическая",
+    "AUTOMATIC": "автоматическая",
+    "ROBOT": "роботизированная",
+    "VARIATOR": "вариатор",
+}
+
+DRIVE_MAP = {
+    "FWD": "передний",
+    "RWD": "задний",
+    "AWD": "полный",
+}
+
+BODY_TYPE_MAP = {
+    "SEDAN": "седан",
+    "HATCHBACK": "хэтчбек 5 дв.",
+    "WAGON": "универсал",
+    "SUV": "внедорожник 5 дв.",
+    "COUPE": "купе",
+    "MINIVAN": "минивэн",
+    "PICKUP": "пикап",
+}
+
+CONDITION_MAP = {
+    "EXCELLENT": "отличное",
+    "GOOD": "хорошее",
+    "SATISFACTORY": "удовлетворительное",
+    "POOR": "плохое",
+}
