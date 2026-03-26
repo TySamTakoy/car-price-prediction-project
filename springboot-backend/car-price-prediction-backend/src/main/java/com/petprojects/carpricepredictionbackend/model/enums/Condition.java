15b067d84e3a0db1559aca636f3f5b40b7a4dd41
TySamTakoy <brat.sumkina@gmail.com>
2026-03-26 14:58:31 +0200

    init: add Condition enum in model

---
+++ springboot-backend/car-price-prediction-backend/src/main/java/com/petprojects/carpricepredictionbackend/model/enums/Condition.java
@@ -0,0 +1,33 @@
+package com.petprojects.carpricepredictionbackend.model.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum Condition {
+    EXCELLENT("Отличное"),
+    GOOD("Хорошее"),
+    SATISFACTORY("Удовлетворительное"),
+    POOR("Плохое");
+
+    private final String displayName;
+
+    Condition(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @JsonValue
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @JsonCreator
+    public static Condition fromValue(String value) {
+        for (Condition type : values()) {
+            if (type.name().equalsIgnoreCase(value) ||
+                    type.displayName.equalsIgnoreCase(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Unknown Condition: " + value);
+    }
+}
