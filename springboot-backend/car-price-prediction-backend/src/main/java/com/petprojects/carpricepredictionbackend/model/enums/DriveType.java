bce11526b5205faf787a8d7908931b54a1f4a570
TySamTakoy <brat.sumkina@gmail.com>
2026-03-26 14:58:41 +0200

    init: add DriveType enum in model

---
+++ springboot-backend/car-price-prediction-backend/src/main/java/com/petprojects/carpricepredictionbackend/model/enums/DriveType.java
@@ -0,0 +1,32 @@
+package com.petprojects.carpricepredictionbackend.model.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum DriveType {
+    FWD("Передний"),
+    RWD("Задний"),
+    AWD("Полный");
+
+    private final String displayName;
+
+    DriveType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @JsonValue
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @JsonCreator
+    public static DriveType fromValue(String value) {
+        for (DriveType type : values()) {
+            if (type.name().equalsIgnoreCase(value) ||
+                    type.displayName.equalsIgnoreCase(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Unknown DriveType: " + value);
+    }
+}
