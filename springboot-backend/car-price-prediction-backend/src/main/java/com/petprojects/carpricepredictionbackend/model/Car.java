26b6ae4062dffc1f4e458d36bfed0b80346bb519
TySamTakoy <brat.sumkina@gmail.com>
2026-03-26 15:16:10 +0200

    init: add Car model

---
+++ springboot-backend/car-price-prediction-backend/src/main/java/com/petprojects/carpricepredictionbackend/model/Car.java
@@ -0,0 +1,81 @@
+package com.petprojects.carpricepredictionbackend.model;
+
+import com.petprojects.car_price_prediction_backend.model.enums.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "cars")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private String model;
+
+    @Column
+    private String generation;
+
+    @Column(nullable = false)
+    private Integer year;
+
+    @Column(nullable = false)
+    private Integer mileage;
+
+    @Column(name = "engine_volume", nullable = false)
+    private Double engineVolume;
+
+    @Column(name = "engine_power", nullable = false)
+    private Double enginePower;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "engine_type", nullable = false)
+    private EngineType engineType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Transmission transmission;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "drive_type", nullable = false)
+    private DriveType driveType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "body_type", nullable = false)
+    private BodyType bodyType;
+
+    @Column(nullable = false)
+    private String color;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Condition condition;
+
+    @Column(name = "owners_count", nullable = false)
+    private Integer ownersCount;
+
+    @Column(columnDefinition = "TEXT")
+    private String complectation;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
