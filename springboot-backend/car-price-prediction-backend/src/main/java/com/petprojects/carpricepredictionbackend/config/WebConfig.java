ce145f1e7b07879935774671f2a668e8900a2dd8
TySamTakoy <66198965+TySamTakoy@users.noreply.github.com>
2026-06-01 21:22:33 +0200

    Update WebConfig.java

--- springboot-backend/car-price-prediction-backend/src/main/java/com/petprojects/carpricepredictionbackend/config/WebConfig.java
+++ springboot-backend/car-price-prediction-backend/src/main/java/com/petprojects/carpricepredictionbackend/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.petprojects.carpricepredictionbackend.config;
+package com.petprojects.car_price_prediction_backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
