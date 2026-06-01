7e9ad7326d5dbf78ac0c8c1f2840ad2cb1c053bf
TySamTakoy <66198965+TySamTakoy@users.noreply.github.com>
2026-06-01 21:22:15 +0200

    Update RestTemplateConfig.java

--- springboot-backend/car-price-prediction-backend/src/main/java/com/petprojects/carpricepredictionbackend/config/RestTemplateConfig.java
+++ springboot-backend/car-price-prediction-backend/src/main/java/com/petprojects/carpricepredictionbackend/config/RestTemplateConfig.java
@@ -1,10 +1,9 @@
-package com.petprojects.carpricepredictionbackend.config;
+package com.petprojects.car_price_prediction_backend.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-
 import java.time.Duration;
 
 @Configuration
