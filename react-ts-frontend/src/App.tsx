915219096411f31c88329c9ac904b99bd3c8b50d
TySamTakoy <66198965+TySamTakoy@users.noreply.github.com>
2026-06-01 20:49:11 +0200

    Add files via upload

---
+++ react-ts-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AppRouter from "./router/AppRouter";
+import Navbar from "./components/Navbar";
+
+function App() {
+    return (
+        <>
+            <Navbar />
+            <AppRouter />
+        </>
+    );
+}
+
+export default App;
