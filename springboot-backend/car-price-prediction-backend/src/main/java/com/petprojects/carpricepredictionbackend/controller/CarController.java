c609b36c058f2f3b96e8e9d9ba48016ebaf4f4b6
TySamTakoy <66198965+TySamTakoy@users.noreply.github.com>
2026-06-01 21:23:40 +0200

    Update CarController.java

--- springboot-backend/car-price-prediction-backend/src/main/java/com/petprojects/carpricepredictionbackend/controller/CarController.java
+++ springboot-backend/car-price-prediction-backend/src/main/java/com/petprojects/carpricepredictionbackend/controller/CarController.java
@@ -1,14 +1,11 @@
-package com.petprojects.carpricepredictionbackend.controller;
+package com.petprojects.car_price_prediction_backend.controller;
 
-import com.petprojects.carpricepredictionbackend.dto.CarRequestDTO;
-import com.petprojects.carpricepredictionbackend.service.CarService;
+import com.petprojects.car_price_prediction_backend.dto.CarRequestDTO;
+import com.petprojects.car_price_prediction_backend.service.CarService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
